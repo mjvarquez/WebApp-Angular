2aca87b3cfba9b423dafeceb82c7b91171695480
mjvarquez <markjeraldv22@gmail.com>
2022-12-01 21:59:57 +0800

    implementing format data for homepage

--- main/src/app/adminpage/views/dashboard/dashboard-components/survey-result/survey-result.component.ts
+++ main/src/app/adminpage/views/dashboard/dashboard-components/survey-result/survey-result.component.ts
@@ -11,40 +11,50 @@ import { OrderMenuService } from 'src/app/store/homepage/order-menu.service';
 export class SurveyResultComponent implements OnInit {
   dataSource!: any;
   displayedColumns: string[] = ['surveyResult'];
-  dateServed = new Date().toISOString().slice(0, 10);
 
   constructor(private orderMenuService: OrderMenuService) { }
 
   getSurveyResult(){
     // let dishVotes: Map<string, any> = new Map<string, any>();
     // let votedDishDetails: Map<string, VotedDishDetails> = new Map<string, VotedDishDetails>();
-    let dishCount: any[] = [];
+    let dishCounts: any[] = [];
     this.orderMenuService.getSurveyData().subscribe({
       next: (res) => {
         res.forEach((dish: any) => {
+          const date = {
+            date_served: dish.date_served,
+          }
           // for dish survey result
           let count: { [key: string ]: any } = {};
-          // let count: any[]
           dish.dishes.forEach((dishes: any) => {
             const dishDetails = {
+              date,
               dishName: dishes.dishName,
               dishType: dishes.dishType,
               price: dishes.price
             }
-            dishCount.push(dishDetails.dishName)
-            // console.log(dishCount)
-            dishCount.forEach((c: any) => {
-              count[c] = (count[c] || 0 ) + 1 
+            // console.log(dishDetails)
+            dishCounts.push(dishDetails)
+            // for(const dishCount of dishCounts){
+            //   const data = `${dishCount.date_served}_${dishCount.dishName}_${dishCount.dishType}_${dishCount.price}`;
+            //   (count[data] || (count[data] = {...dishCount, count: 0})).count += 1;
+            // }
+            // const result = Object.values(count);
+            // result.length = 5;
+            // const dishSorted = Object.values(result).sort(function(a,b){return count[b] - count[a]});
+ 
+            dishCounts.forEach((c: any) => {
+              count[c.dishName] = (count[c.dishName] || 0 ) + 1 
             });
-            // console.log(count)
+            console.log(count)
             let dishSorted = Object.keys(count).sort(function(a,b){return count[b] - count[a]});
             dishSorted.length = 5;
+            // console.log(dishSorted)
             this.dataSource = new MatTableDataSource(dishSorted);
             // for menu of the day
             
             // this.orderMenuService.addMenuForToday(selectedMenu)
             // console.log(this.menuForToday)
-            
           })
         })
       }
